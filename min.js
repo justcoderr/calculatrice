5da7e665c016e9cb1c104323a3e86588d61fa547
aelhajji <elhajjiabdeljabar@gmail.com>
2021-12-17 20:51:34 +0100

    add  js

--- min.js
+++ min.js
@@ -1,4 +1,64 @@
 let audio = document.getElementsByTagName('audio')[0];
+var btn = Array.from(document.getElementsByTagName("td"));
+var text = document.getElementsByClassName('text')[0]
+console.log(btn);
+
+btn.map(clikedtn =>{
+    clikedtn.addEventListener('click' , (e) =>{
+        switch(e.target.innerText){
+            case 'on/c':
+                this.text.innerText = "0";
+                break;
+            case '×':
+                this.text.innerText += "*";
+                break;
+            case '÷':
+                this.text.innerText += "/";
+                break;
+            // todo
+            case 'off':
+                this.text.innerText = this.text.innerText;
+                break;
+            case '%':
+                this.text.innerText = this.text.innerText;
+                break;
+            case 'tax':
+                this.text.innerText = this.text.innerText;
+                break;
+            case 'm+':
+                this.text.innerText = this.text.innerText;
+                break;
+            case 'm-':
+                this.text.innerText = this.text.innerText;
+                break; 
+            case '+/-':
+                this.text.innerText = this.text.innerText;
+                break; 
+            case 'CE':
+                if(text.innerText == "Error!"){
+                    text.innerText = "0";
+                }
+                else{
+                    text.innerText = text.innerText.slice(0, -1);
+                }
+                break
+            case '=':
+                try{
+                    text.innerText= eval(text.innerText);
+                }catch{
+                    text.innerText= "Error!";
+                }
+                break;
+            default:
+                if(text.innerText == "0"){
+                    text.innerText = e.target.innerText;
+                }else{
+                    text.innerText += e.target.innerText;
+                }
+        }
+    })
+})
+
 
 document.getElementsByTagName("table")[0].addEventListener("click" , function(){
     audio.play()
